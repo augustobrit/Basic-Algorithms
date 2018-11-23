72bd76a4ec21fb13521d8a241f8a9831982c6b86
Augusto <augustobrito@outlook.com>
2018-11-23 03:59:20 -0200

    Update Readme

---
+++ Conditional/FillWall.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+#define A 2;
+#define B 3;
+#define C 5;
+
+int main() {
+    float area;
+    int a;
+    int b;
+    int c;
+
+    printf("Type Wall Area: ");
+    scanf("%f", &area);
+
+    a = area / (A*A);
+}
--- Conditional/ShapeArea.c
+++ Conditional/ShapeArea.c
@@ -1,5 +1,43 @@
 #include <stdio.h>
 
 int main () {
-      
+    int opt = 0;
+    float area = 0;
+    float b;
+    float h;
+    float bMinor;
+    float bMajor;
+    float r;
+    float height;
+
+    printf("Choose an option: 1 - Triangle Area | 2 - Circle Area | 3 - Trapezium Area");
+    scanf("%d", &opt);
+
+    switch(opt) {
+        case 1 :
+            printf("Type Triangle Base: ");
+            scanf("%f", &b);
+            printf("Type Triangle Height: ");
+            scanf("%f", &h);
+            area = b * h / 2;
+            break;
+        case 2 :
+            printf("Type Radius: ");
+            scanf("%f", &r);
+            area = 3.14 * r * r;
+            break;
+        case 3 :
+            printf("Type b: ");
+            scanf("%f", &bMinor);
+            printf("Type B: ");
+            scanf("%f", &bMajor);
+            printf("Type Height: ");
+            scanf("%f", &height);
+            area = ((bMinor+bMajor)*height)/2;
+            break;
+        default :
+            printf("Invalid Option");
+    }
+
+    printf("Area = %f", area);
 }
